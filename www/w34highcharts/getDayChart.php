1de18a8384762cc705aea4a501204f5a1d5ebfe1
Ian Millard <steepleian@gmail.com>
2020-05-14 23:45:19 +0100

    Update getDayChart.php

--- www/w34highcharts/getDayChart.php
+++ www/w34highcharts/getDayChart.php
@@ -10,6 +10,7 @@
     <script src="https://code.highcharts.com/stock/highstock.js"></script>
     <script src="https://code.highcharts.com/stock/highcharts-more.js"></script>
     <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/windbarb.js"></script>
     <script src="https://code.highcharts.com/modules/boost.js"></script>    
     <script src="scripts/<?php echo $theme1;?>-theme.js" type="text/javascript"></script>
     <script src="scripts/plots_config.js" type="text/javascript"></script>
@@ -25,21 +26,50 @@
     </body> 
 </html>
 <?php
-    putenv("PYTHONPATH=".$_GET['weewxpathbin']);
+    if (isset($weexserver_address) and isset($weexserver_port)){
+      $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+      socket_connect($socket, $weexserver_address, $weexserver_port);
+    }
+    else
+      putenv("PYTHONPATH=".$_GET['weewxpathbin']);
     $plot_info = explode(",",$_GET['plot_type']);
     $units = explode(",",$_GET['units']);
-    unlink($plot_info[1]);
-    #print($plot_info[2]." ".$_GET['epoch']." ".$plot_info[1].".tmpl ".getcwd());
+    if (file_exists($plot_info[1]))
+      unlink($plot_info[1]);
+    $cmd = $plot_info[2]." ".$_GET['epoch']." ".$plot_info[1].".tmpl ".getcwd();
+    #print($cmd);
     if (isset($_GET['epoch1'])){
-      unlink($plot_info[3]);
-      $output = shell_exec(escapeshellcmd($plot_info[2]." ".$_GET['epoch']." ".$plot_info[1].".tmpl ".getcwd()));
+      if (file_exists($plot_info[3]))
+        unlink($plot_info[3]);
+      if (isset($weexserver_address) and isset($weexserver_port)) {
+        socket_write($socket, $cmd, strlen($cmd));
+        @socket_read($socket, 1, PHP_NORMAL_READ);
+        socket_close($socket);
+      }
+      else
+        $output = shell_exec(escapeshellcmd($cmd));
       rename($plot_info[1], $plot_info[3]);
-      #print($plot_info[2]." ".$_GET['epoch1']." ".$plot_info[1].".tmpl ".getcwd());
-      $output = shell_exec(escapeshellcmd($plot_info[2]." ".$_GET['epoch1']." ".$plot_info[1].".tmpl ".getcwd()));
+      $cmd = $plot_info[2]." ".$_GET['epoch1']." ".$plot_info[1].".tmpl ".getcwd();
+      #print($cmd);
+      if (isset($weexserver_address) and isset($weexserver_port)) {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_connect($socket, $weexserver_address, $weexserver_port);
+        socket_write($socket, $cmd, strlen($cmd));
+        @socket_read($socket, 1, PHP_NORMAL_READ);
+        socket_close($socket);
+      }
+      else
+        $output = shell_exec(escapeshellcmd($cmd));
       echo "<script> display_chart({temp:"."'".$units[0]."',pressure:"."'".$units[1]."',wind:"."'".$units[2]."',rain:"."'".$units[3]."'},'".$plot_info[0]."','weekly',false,true,'".$plot_info[4]."',".$plot_info[5].");</script>";
     }
     else {
-      $output = shell_exec(escapeshellcmd($plot_info[2]." ".$_GET['epoch']." ".$plot_info[1].".tmpl ".getcwd()));
+      if (isset($weexserver_address) and isset($weexserver_port)){
+        socket_write($socket, $cmd, strlen($cmd));
+        @socket_read($socket, 1, PHP_NORMAL_READ);
+        socket_close($socket);
+      }
+      else
+        $output = shell_exec(escapeshellcmd($cmd));
       echo "<script> display_chart({temp:"."'".$units[0]."',pressure:"."'".$units[1]."',wind:"."'".$units[2]."',rain:"."'".$units[3]."'},'".$plot_info[0]."','weekly',true,false,'".$plot_info[3]."',".$plot_info[4].");</script>";
     }
 ?> 
